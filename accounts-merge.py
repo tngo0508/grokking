2fedf9643d5c53ea2632d83af0e69716cd73b0d6
Thomas Ngo <tngo0508@gmail.com>
2023-11-20 20:54:39 -0800

    practice trie + disjoint set

---
+++ accounts-merge.py
@@ -0,0 +1,116 @@
+from collections import defaultdict
+
+class UnionFind():
+    def __init__(self, n) -> None:
+        self.root = [i for i in range(n)]
+        self.rank = [1] * n
+    
+    def find(self, x):
+        if self.root[x] == x:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = self.root[rootY]
+            elif self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = self.root[rootX]
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+
+class Solution:
+    def accountsMerge(self, accounts: List[List[str]]) -> List[List[str]]:
+        uf = UnionFind(len(accounts))
+        email_to_idx = defaultdict(int)
+        for i, acc in enumerate(accounts):
+            for email in acc[1:]:
+                if email not in email_to_idx:
+                    email_to_idx[email] = i
+                else:
+                    uf.union(i, email_to_idx[email])
+
+        for i in range(len(accounts)):
+            uf.find(i)
+
+        output = []
+        
+        merge_acc = defaultdict(set)
+
+        for i, root_idx in enumerate(uf.root):
+            name = accounts[root_idx][0]
+            for email in accounts[root_idx][1:]:
+                merge_acc[(root_idx, name)].add(email)
+            for email in accounts[i][1:]:
+                merge_acc[(root_idx, name)].add(email)
+
+        for (_, name), set_email in merge_acc.items():
+            emails = list(set_email)
+            emails.sort()
+            output.append([name] + emails)
+
+        return output
+    
+# SOLUTION
+from union_find import *
+from collections import defaultdict
+
+def accounts_merge(accounts):
+
+    uf = UnionFind(len(accounts))
+    email_mapping = {}
+
+    for i, account in enumerate(accounts):
+        emails = account[1:]
+
+        for email in emails:
+            if email in email_mapping:
+                if account[0] != accounts[email_mapping[email]][0]: 
+                    return
+                uf.union(email_mapping[email], i)
+
+            email_mapping[email] = i
+        
+    merged_accounts = defaultdict(list)
+    for email, ids in email_mapping.items():
+        merged_accounts[uf.find(ids)].append(email)
+
+    final_merged =[]
+    
+    for parent, emails in merged_accounts.items():
+        final_merged.append([accounts[parent][0]]+sorted(emails))
+    return final_merged
+
+# driver code
+def main():
+    all_accounts = [[["Emma", "emma@mail.com", "emma_work@mail.com"], ["Bob", "bob_home@mail.com", "bob123@mail.com"], ["Emma", "emma_art@mail.com", "emma_work@mail.com"], ["Bob", "bob321@mail.com"]],
+                    [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]],
+                    [["Sarah", "sarah@mail.com", "sh@mail.com"],["Sarah", "sarah1@mail.com", "sarahh@mail.com"], ["Sarah", "sh3@mail.com"]],
+                    [["Alice", "alice@mail.com"], ["Alice", "alice_alice@mail.com", "alice@mail.com"], ["Alice", "alice@mail.com", "alice123@mail.com", "aalicee@mail.com"]],
+                    [["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]]
+    
+    for i in range(len(all_accounts)):
+        print(i+1, ".\tAccounts: \n\t[", sep="")
+        for a in all_accounts[i]:
+            print("\t",a)
+        print("\t]")
+        
+        merged = accounts_merge(all_accounts[i])
+        
+        if(merged == None):
+            print("Error!\nAccounts sharing some email(s) should have the same names.")
+            return
+
+        print("\n\tMerged accounts: \n\t[")
+        for m in merged:
+            print("\t",m)
+        print("\t]")
+        print("-" * 100)
+
+if __name__ == '__main__':
+        main()
--- lexicographical-numbers.py
+++ lexicographical-numbers.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 class TrieNode():
     def __init__(self):
         self.children = {}
@@ -56,3 +59,27 @@ def lexicographical_order(n):
 
 print(lexicographical_order(12))
 print(lexicographical_order(15))
+
+def lexicalOrderOptimalSolution(n: int) -> List[int]:
+        res = []
+        cur = 1
+        for i in range(n):
+            # Add the current number to the result
+            res.append(cur)
+            # If the current number times 10 is less than or equal to n, move to the next level
+            if cur * 10 <= n:
+                cur *= 10
+            # Otherwise, move to the next number at the current level
+            else:
+                # If we've reached the end of a level, divide by 10 to go back up one level
+                if cur >= n:
+                    cur //= 10
+                cur += 1
+                # Skip over any trailing zeroes (e.g. if cur = 19, skip over 190, 191, 192, etc.)
+                while cur % 10 == 0:
+                    cur //= 10
+        return res
+
+print(lexicalOrderOptimalSolution(12))
+print(lexicalOrderOptimalSolution(15))
+print(lexicalOrderOptimalSolution(115))
