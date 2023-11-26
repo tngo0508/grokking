ba1df4419cc9968e4269edbbe0a4758a11c84c39
Thomas Ngo <tngo0508@gmail.com>
2023-11-25 21:28:58 -0800

    comment

--- encode-and-decode-strings.py
+++ encode-and-decode-strings.py
@@ -12,43 +12,54 @@ def encode(strings):
     encoded_string = ""
 
     for x in strings:
+        # Add appended 4 byte string length to string in encoded string
         encoded_string += length_to_bytes(x) + x
 
     return encoded_string
 
 
 def decode(string):
+    # Initialize a pointer
     i = 0
     decoded_string = []
 
     while i < len(string):
+        # Get length in integer from 4 byte string
         length = bytes_to_length(string[i : i + 4])
         i += 4
+        # Add string of the computed length
         decoded_string.append(string[i : i + length])
+        # Move the pointer
         i += length
 
     return decoded_string
 
 
 def length_to_bytes(x):
-    length = len(x) 
+    # Express the string length as a string of bytes
+    # If there are 4 characters in the string, the function will return '0004'
+    length = len(x) # Compute the length of string
     bytes = []
 
     for i in range(4):
+        # Apply right shift operator
         bytes.append(chr(length >> (i * 8)))
 
     bytes.reverse()
+    # Convert the array to the string
     bytes_string = "".join(bytes)
 
     return bytes_string
 
 
 def bytes_to_length(bytes_string):
+    # Convert a 4-byte string to an integer
+    # If bytes_string is '0040', the function will return 40
     result = 0
 
     for c in bytes_string:
         result = result * 256 + ord(c)
-
+        
     return result
 
 
