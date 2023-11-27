bb64351f94fe2660c9f8ad092f43be0b3683c6a5
Thomas Ngo <tngo0508@gmail.com>
2023-11-27 07:01:09 -0800

    understand my mistakes

--- shortest-bridge.py
+++ shortest-bridge.py
@@ -1,3 +1,4 @@
+# TIME LIMIT EXCEEDED
 from collections import deque
 
 def mark_island(row, col, grid, val):
@@ -64,6 +65,47 @@ def shortest_bridge(grid):
     
     return result if result != float('inf') else 0
 
+# IMPROVED - PASS TEST CASES
+class Solution:
+    def shortestBridge(self, grid: List[List[int]]) -> int:
+        num_rows = len(grid)
+        num_cols = len(grid[0])
+        r, c = -1, -1
+        queue = deque()
+
+        for row in range(num_rows):
+            for col in range(num_cols):
+                if grid[row][col] == 1:
+                    r, c = row, col
+                    break
+            if r != -1:
+                break
+
+        def mark_island_A(curr_row, curr_col, queue):
+            grid[curr_row][curr_col] = '#'
+            queue.append([curr_row, curr_col, 0])
+            for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                row, col = curr_row + x, curr_col + y
+                if 0 <= row < num_rows and 0 <= col < num_cols:
+                    if grid[row][col] == 1:
+                        mark_island_A(row, col, queue)
+                
+        
+        mark_island_A(r, c, queue)
+        while queue:
+            curr_row, curr_col, dist = queue.popleft()
+            for x, y in [(0, 1), (1, 0), (-1, 0), (0, -1)]:
+                row, col = curr_row + x, curr_col + y
+                
+                if 0 <= row < num_rows and 0 <= col < num_cols:
+                    if grid[row][col] == 1:
+                        return dist
+                    elif grid[row][col] == 0:
+                        queue.append([row, col, dist + 1])
+                        grid[row][col] = -1
+        
+        return 0
+
 
 print(shortest_bridge([[0,1,1],[1,0,1],[1,0,1]]))
 print(shortest_bridge([[0, 1, 1, 1], [1, 0, 1, 1], [1, 1, 0, 1], [1, 1, 1, 0]]))
@@ -71,3 +113,5 @@ print(shortest_bridge([[1, 0, 0], [1, 0, 0], [0, 0, 1]]))
 print(shortest_bridge([[1, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 1]]))
 print(shortest_bridge(
 [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [1, 1, 1, 1, 1]]))
+
+
