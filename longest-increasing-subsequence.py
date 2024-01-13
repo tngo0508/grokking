1cf4191e7ac483f7cedb811066829c7746e1b604
Thomas Ngo <tngo0508@gmail.com>
2024-01-13 02:40:37 -0800

    practice dp

--- longest-increasing-subsequence.py
+++ longest-increasing-subsequence.py
@@ -64,3 +64,14 @@ class Solution:
         return max(dp)
     
 
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        dp = [1] * N
+        for i in range(N):
+            for j in range(i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+                
+        return max(dp)
+
