2f12bc86d1972ca589371dcb7ef0e588ebd546f9
Thomas Ngo <tngo0508@gmail.com>
2024-01-13 01:40:54 -0800

    practice dp

--- longest-increasing-subsequence.py
+++ longest-increasing-subsequence.py
@@ -1,3 +1,55 @@
+# Brute force - TLE
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        result = 0
+
+        def dfs(index, curr_num):
+            if index == N:
+                return 0
+
+            max_length = 0
+            for i in range(index + 1, N):
+                if curr_num < nums[i]:
+                    length = dfs(i, nums[i]) + 1
+                    max_length = max(max_length, length)
+
+            return max_length
+        
+        
+        for i in range(N):
+            result = max(result, dfs(i, nums[i]) + 1)
+        return result
+    
+# Memoization - Accepted
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        N = len(nums)
+        result = 0
+        memo = defaultdict()
+
+        def dfs(index, curr_num):
+            if index == N:
+                return 0
+
+            if (index, curr_num) in memo:
+                return memo[(index, curr_num)]
+
+            max_length = 0
+            for i in range(index + 1, N):
+                if curr_num < nums[i]:
+                    length = dfs(i, nums[i]) + 1
+                    max_length = max(max_length, length)
+
+            memo[(index, curr_num)] = max_length
+            return max_length
+        
+        
+        for i in range(N):
+            result = max(result, dfs(i, nums[i]) + 1)
+        return result
+
+# Dynamic programming
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         N = len(nums)
@@ -10,3 +62,5 @@ class Solution:
                 j -= 1
         
         return max(dp)
+    
+
