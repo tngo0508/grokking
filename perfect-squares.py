a586103d1ac3d26e771d2b6f50d60835862c90d7
Thomas Ngo <tngo0508@gmail.com>
2024-01-12 16:26:35 -0800

    practice dp

--- perfect-squares.py
+++ perfect-squares.py
@@ -1,4 +1,4 @@
-# Brute Force
+# Brute Force - TLE
 class Solution:
     def numSquares(self, n: int) -> int:
         def dfs(num, curr_sum):
@@ -16,3 +16,70 @@ class Solution:
             return result
 
         return dfs(0, n)
+    
+# Memoization - TLE
+import math
+class Solution:
+    def numSquares(self, n: int) -> int:
+        memo = defaultdict()
+        def dfs(curr_sum):
+            if curr_sum == 0:
+                return 0
+
+            if curr_sum < 0:
+                return float('inf')
+
+            if curr_sum in memo:
+                return memo[curr_sum]
+
+            result = float('inf')
+            for x in range(1, math.ceil(curr_sum / 2) + 1):
+                square = x **2
+                result = min(result, dfs(curr_sum - square) + 1)
+
+            memo[curr_sum] = result
+            return result
+
+        return dfs(n)
+    
+# Dynamic Programming - Bottom-up Approach
+import math
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [n] * (n + 1)
+        dp[0] = 0
+        x = 1
+        while x ** 2 <= n:
+            dp[x**2] = 1
+            x += 1
+        
+        closest_square = [1]
+        for i in range(1, n + 1):
+            if dp[i] == 1:
+                closest_square.append(i)
+            else:
+                for square in closest_square:
+                    dp[i] = min(dp[i], dp[i - square] + dp[square])
+
+        return dp[-1]
+
+# Editorial Solution
+class Solution(object):
+    def numSquares(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        square_nums = [i**2 for i in range(0, int(math.sqrt(n))+1)]
+        
+        dp = [float('inf')] * (n+1)
+        # bottom case
+        dp[0] = 0
+        
+        for i in range(1, n+1):
+            for square in square_nums:
+                if i < square:
+                    break
+                dp[i] = min(dp[i], dp[i-square] + 1)
+        
+        return dp[-1]
