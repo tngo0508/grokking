6324167e1d5baad27f75cd88ff58e2b7a2b9e97c
Thomas Ngo <tngo0508@gmail.com>
2024-01-23 14:12:09 -0800

    practice

---
+++ merge-k-sorted-lists.py
@@ -0,0 +1,32 @@
+# Brute Force - TLE
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        dummy = ListNode()
+        curr = dummy
+        N = len(lists)
+        hash_map = {}
+        for i in range(N):
+            hash_map[i] = lists[i]
+
+        while curr:
+            temp = []
+            for i in range(N):
+                curr_head = hash_map[i]
+                if curr_head:
+                    temp.append([curr_head.val, curr_head, i])
+            
+            if temp:
+                temp.sort(key=lambda x: x[0])
+                _, node, idx = temp[0]
+                hash_map[idx] = node.next
+                next_node = node
+                curr.next = next_node
+                next_node.next = None
+            curr = curr.next
+        
+        return dummy.next
