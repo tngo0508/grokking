c514c717752f7fe407b49b198bd701a3fa7c56a2
Thomas Ngo <tngo0508@gmail.com>
2024-01-28 13:57:29 -0800

    daily practice

--- product-of-array-except-self.py
+++ product-of-array-except-self.py
@@ -1,3 +1,22 @@
+# use 2 arrays
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        L = [1]
+        for num in nums:
+            L.append(L[-1] * num)
+
+        R = [1]
+        for num in reversed(nums):
+            R.append(R[-1] * num)
+
+        R.reverse()
+
+        res = []
+        for i in range(len(nums)):
+            res.append(L[i] * R[i + 1])
+        return res
+    
+# use 1 array
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         N = len(nums)
