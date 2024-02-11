8929e6e88b6e78ee71c1fcbf9fb380b51f552c18
Thomas Ngo <tngo0508@gmail.com>
2024-02-11 14:21:41 -0800

    practice coding

---
+++ reverse-words-in-a-string.py
@@ -0,0 +1,17 @@
+# My solution
+class Solution:
+    def reverseWords(self, s: str) -> str:
+        res = []
+        N = len(s)
+        curr = []
+        for i in reversed(range(N)):
+            if not s[i].isalnum() and curr:
+                res.append(''.join(curr[::-1]))
+                curr = []
+            
+            if s[i].isalnum():
+                curr.append(s[i])
+        
+        if curr:
+            res.append(''.join(curr[::-1]))
+        return ' '.join(res)
