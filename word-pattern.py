c2c810fd9b28831bdf939d2bae6007904b5e740c
Thomas Ngo <tngo0508@gmail.com>
2024-02-23 16:40:11 -0800

    update

--- word-pattern.py
+++ word-pattern.py
@@ -21,7 +21,25 @@ class Solution:
             if c not in hash_map:
                 return False
             res.append(hash_map[c])
-        
+
         return res == ss
 
-        
+
+# My different implementation
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        p_dict = defaultdict(list)
+        s_dict = defaultdict(list)
+        ss = s.split()
+
+        for i, c in enumerate(pattern):
+            p_dict[c].append(i)
+
+        for i, w in enumerate(ss):
+            s_dict[w].append(i)
+
+        for i, c in enumerate(pattern):
+            if i >= len(ss) or p_dict[c] != s_dict[ss[i]]:
+                return False
+
+        return True
