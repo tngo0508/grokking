0ec0856ec0b51c3c70db929293a75256715082ec
Thomas Ngo <tngo0508@gmail.com>
2024-02-24 18:01:02 -0800

    update

--- greatest-common-divisor-traversal.py
+++ greatest-common-divisor-traversal.py
@@ -1,4 +1,4 @@
-# UnionFind - TLE
+# UnionFind - Memory Limit Exceeded
 class UnionFind:
     def __init__(self, n):
         self.root = [i for i in range(n)]
@@ -40,6 +40,51 @@ class Solution:
         uf = UnionFind(N)
         for x, y in edges:
             uf.union(x, y)
+        
+        for i in range(N):
+            uf.find(i)
+
+        
+        return len(set(uf.root)) == 1
+    
+# UnionFind - Time Limit Exceeded
+class UnionFind:
+    def __init__(self, n):
+        self.root = [i for i in range(n)]
+        self.rank = [0] * n
+
+    def find(self, x):
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            elif self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1 
+
+
+class Solution:
+    def canTraverseAllPairs(self, nums: List[int]) -> bool:
+        def gcd(a, b): # Euclidean Algorithm 
+            while b:
+                a, b = b, a % b
+            return a
+
+        N = len(nums)
+        uf = UnionFind(N)
+        for i in range(N):
+            for j in range(i + 1, N):
+                if gcd(nums[i], nums[j]) > 1:
+                    uf.union(i, j)
         
         for i in range(N):
             uf.find(i)
