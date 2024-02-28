dc11f4208d08fc0d77523df89129baf20a43985a
Thomas Ngo <tngo0508@gmail.com>
2024-02-27 16:09:39 -0800

    problem of the day

---
+++ find-bottom-left-tree-value.py
@@ -0,0 +1,46 @@
+# My solution
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        queue = deque()
+        queue.append(root)
+        res = root
+        while queue:
+            N = len(queue)
+            for i in range(N):
+                node = queue.popleft()
+                if i == 0:
+                    res = node
+                if node:
+                    if node.left:
+                        queue.append(node.left)
+                    if node.right:
+                        queue.append(node.right)
+        
+        return res.val
+                
+# Approach 1: Depth-First Search
+class Solution:
+    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
+        self.maxDepth = -1
+        self.bottomLeftValue = 0
+        self.dfs(root, 0)
+        return self.bottomLeftValue
+
+    def dfs(self, current: TreeNode, depth: int):
+        if not current:
+            return
+        
+        if depth > self.maxDepth:  # If true, we discovered a new level
+            self.maxDepth = depth
+            self.bottomLeftValue = current.val
+
+        self.dfs(current.left, depth + 1)
+        self.dfs(current.right, depth + 1)
+        return
+
