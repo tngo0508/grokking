e9ad8e7de03c59278edc1f9a2044570cecf5daca
Thomas Ngo <tngo0508@gmail.com>
2024-03-07 12:39:59 -0800

    review

--- reverse-words-in-a-string.py
+++ reverse-words-in-a-string.py
@@ -15,3 +15,34 @@ class Solution:
         if curr:
             res.append(''.join(curr[::-1]))
         return ' '.join(res)
+    
+# Other approach
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        temp = []
+        curr = head
+        while curr:
+            temp.append(curr)
+            curr = curr.next
+        
+        for i in range(0, len(temp), k):
+            l, r = i, i + k - 1
+            if r >= len(temp):
+                break
+            while l < r:
+                temp[l], temp[r] = temp[r], temp[l]
+                l += 1
+                r -= 1
+        
+        for i in range(len(temp) - 1):
+            temp[i].next = temp[i + 1]
+
+        if temp:
+            temp[-1].next = None
+        
+        return temp[0] if temp else None
