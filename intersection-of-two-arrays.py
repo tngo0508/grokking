387a938daa4c219f2d75d3a88ac21a089416d5e6
Thomas Ngo <tngo0508@gmail.com>
2024-03-09 16:19:25 -0800

    review

--- intersection-of-two-arrays.py
+++ intersection-of-two-arrays.py
@@ -1,10 +1,26 @@
+# My solution
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        num1_set = set(nums1)
+        hash_map = defaultdict(int)
+        for num in nums1:
+            hash_map[num] = 1
         res = []
         
         for num in nums2:
-            if num in num1_set and num not in res:
+            if hash_map[num] == 1:
                 res.append(num)
+                hash_map[num] -= 1
         
         return res
+
+# Approach 2: Built-in Set Intersection
+class Solution:
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """  
+        set1 = set(nums1)
+        set2 = set(nums2)
+        return list(set2 & set1)
